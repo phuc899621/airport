723a2a1515f911324861a04c069d3f49afdffd92
phuc899621 <phuc899621@gmail.com>
2025-10-30 09:23:50 +0700

    first config

---
+++ index.js
@@ -0,0 +1,17 @@
+// server.js
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+// Route mẫu
+app.get("/", (req, res) => {
+  res.send("Hello from Express with ESM!");
+});
+
+// Lắng nghe
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running at http://localhost:${PORT}`);
+});
