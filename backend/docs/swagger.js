c908196673514847bfb21e3c7f3c175f798c22f7
phuc899621 <phuc899621@gmail.com>
2025-11-30 18:53:08 +0700

    deloy fix 18

--- backend/docs/swagger.js
+++ backend/docs/swagger.js
@@ -1,6 +1,7 @@
 import swaggerJsDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
 
+
 const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
@@ -22,11 +23,14 @@ const swaggerOptions = {
   },
   apis: ['./routes/*.js', './docs/*.js'], 
 };
-
+const CSS_URL =
+  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 const setupSwagger = (app) => {
-  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+  app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs,{
+    customCssUrl: CSS_URL,
+  }));
 };
 
 export default setupSwagger;
